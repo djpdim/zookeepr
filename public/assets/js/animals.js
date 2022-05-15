ee07d0ea38ac7f66ff5860b64e51bf191b069495
Dimosthenis Pagkas <dimosthenispagkas@MacBook-Pro.fios-router.home>
2022-05-15 14:04:40 -0400

    final code corrections

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,63 +1,78 @@
-const fs = require("fs")
-const path = require("path")
+const $animalForm = document.querySelector("#animals-form")
+const $displayArea = document.querySelector("#display-area")
 
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = []
-    let filteredResults = animalsArray
-    if (query.personalityTraits) {
-        if (typeof query.personalityTraits === "string") {
-            personalityTraitsArray = [query.personalityTraits]
-        } else {
-            personalityTraitsArray = query.personalityTraits
-        }
-        console.log(personalityTraitsArray)
-        personalityTraitsArray.forEach(trait => {
-            filteredResults = filteredResults.filter(animal => animal.personalityTraits.indexOf(trait) !== -1)
+const printResults = resultArr => {
+    console.log(resultArr)
+
+    const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
+        return `
+  <div class="col-12 col-md-5 mb-3">
+    <div class="card p-3" data-id=${id}>
+      <h4 class="text-primary">${name}</h4>
+      <p>Species: ${species.substring(0, 1).toUpperCase() + species.substring(1)}<br/>
+      Diet: ${diet.substring(0, 1).toUpperCase() + diet.substring(1)}<br/>
+      Personality Traits: ${personalityTraits
+          .map(trait => `${trait.substring(0, 1).toUpperCase() + trait.substring(1)}`)
+          .join(", ")}</p>
+    </div>
+  </div>
+    `
+    })
+
+    $displayArea.innerHTML = animalHTML.join("")
+}
+
+const getAnimals = (formData = {}) => {
+    let queryUrl = "/api/animals?"
+
+    Object.entries(formData).forEach(([key, value]) => {
+        queryUrl += `${key}=${value}&`
+    })
+
+    console.log(queryUrl)
+
+    fetch(queryUrl)
+        .then(response => {
+            if (!response.ok) {
+                return alert("Error: " + response.statusText)
+            }
+            return response.json()
         })
-    }
-    if (query.diet) {
-        filteredResults = filteredResults.filter(animal => animal.diet === query.diet)
-    }
-    if (query.species) {
-        filteredResults = filteredResults.filter(animal => animal.species === query.species)
-    }
-    if (query.name) {
-        filteredResults = filteredResults.filter(animal => animal.name === query.name)
-    }
-    return filteredResults
+        .then(animalData => {
+            console.log(animalData)
+            printResults(animalData)
+        })
 }
 
-function findById(id, animalsArray) {
-    const result = animalsArray.filter(animal => animal.id === id)[0]
-    return result
+const handleGetAnimalsSubmit = event => {
+    event.preventDefault()
+    const dietRadioHTML = $animalForm.querySelectorAll('[name="diet"]')
+    let diet
+
+    for (let i = 0; i < dietRadioHTML.length; i += 1) {
+        if (dietRadioHTML[i].checked) {
+            diet = dietRadioHTML[i].value
+        }
+    }
+
+    if (diet === undefined) {
+        diet = ""
+    }
+
+    const personalityTraitArr = []
+    const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions
+
+    for (let i = 0; i < selectedTraits.length; i += 1) {
+        personalityTraitArr.push(selectedTraits[i].value)
+    }
+
+    const personalityTraits = personalityTraitArr.join(",")
+
+    const animalObject = { diet, personalityTraits }
+
+    getAnimals(animalObject)
 }
 
-function createNewAnimal(body, animalsArray) {
-    const animal = body
-    animalsArray.push(animal)
-    fs.writeFileSync(path.join(__dirname, "../data/animals.json"), JSON.stringify({ animals: animalsArray }, null, 2))
-    return animal
-}
+$animalForm.addEventListener("submit", handleGetAnimalsSubmit)
 
-function validateAnimal(animal) {
-    if (!animal.name || typeof animal.name !== "string") {
-        return false
-    }
-    if (!animal.species || typeof animal.species !== "string") {
-        return false
-    }
-    if (!animal.diet || typeof animal.diet !== "string") {
-        return false
-    }
-    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
-        return false
-    }
-    return true
-}
-
-module.exports = {
-    filterByQuery,
-    findById,
-    createNewAnimal,
-    validateAnimal,
-}
+getAnimals()
